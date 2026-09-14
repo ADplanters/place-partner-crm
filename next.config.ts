5cf5a3eb65686e2f6aeb0eabf70b91a174087112
ADplanters <hhjhhj422@gmail.com>
2026-09-14 22:54:23 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 루트 주소(/) 접속 시 자동으로 /rank-check 경로로 영구 이동시키는 설정
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/rank-check',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
